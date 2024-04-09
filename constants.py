56c167d84a16ec4da5a5d9768300f394fb55642e
zhaoch23 <c233zhao@uwaterloo.ca>
2024-04-09 11:29:10 -0400

    Initial commit

---
+++ constants.py
@@ -0,0 +1,7 @@
+from math import pi
+import os
+
+UR10E_XML_DIR = './assets'
+MODEL_ASSET_PATH = "./gesture_recognizer.task"
+UR10E_START_POS = [0.0, -pi / 2, pi / 2, 0.0, 0.0, 0.0]
+SCENE_XML_PATH = os.path.join(UR10E_XML_DIR, "scene.xml")
